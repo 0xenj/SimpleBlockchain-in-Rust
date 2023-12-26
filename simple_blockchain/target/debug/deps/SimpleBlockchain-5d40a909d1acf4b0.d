0ada297f74f76e76c983e6171c2d18e4d5ac84d8
Enj <99920180+0xenj@users.noreply.github.com>
2023-12-26 16:28:11 +0100

    add setup

---
+++ simple_blockchain/target/debug/deps/SimpleBlockchain-5d40a909d1acf4b0.d
@@ -0,0 +1,5 @@
+C:\Users\enzoj\OneDrive\Documents\GitHub\SimpleBlockchain-in-Rust\SimpleBlockchain\target\debug\deps\libSimpleBlockchain-5d40a909d1acf4b0.rmeta: src\main.rs
+
+C:\Users\enzoj\OneDrive\Documents\GitHub\SimpleBlockchain-in-Rust\SimpleBlockchain\target\debug\deps\SimpleBlockchain-5d40a909d1acf4b0.d: src\main.rs
+
+src\main.rs:
---
+++ simple_blockchain/target/debug/deps/SimpleBlockchain-985ad4080fe587ca.d
@@ -0,0 +1,5 @@
+C:\Users\enzoj\OneDrive\Documents\GitHub\SimpleBlockchain-in-Rust\SimpleBlockchain\target\debug\deps\libSimpleBlockchain-985ad4080fe587ca.rmeta: src\main.rs
+
+C:\Users\enzoj\OneDrive\Documents\GitHub\SimpleBlockchain-in-Rust\SimpleBlockchain\target\debug\deps\SimpleBlockchain-985ad4080fe587ca.d: src\main.rs
+
+src\main.rs:
