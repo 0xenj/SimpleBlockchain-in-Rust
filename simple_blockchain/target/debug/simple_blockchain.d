082f8cc75c69979a60a1efb8199979b4f6d549b8
Enj <99920180+0xenj@users.noreply.github.com>
2023-12-27 12:27:58 +0100

    log test

--- simple_blockchain/target/debug/simple_blockchain.d
+++ simple_blockchain/target/debug/simple_blockchain.d
@@ -1,1 +1,1 @@
-C:\Users\enzoj\OneDrive\Documents\GitHub\SimpleBlockchain-in-Rust\simple_blockchain\target\debug\simple_blockchain.exe: C:\Users\enzoj\OneDrive\Documents\GitHub\SimpleBlockchain-in-Rust\simple_blockchain\src\main.rs
+C:\Users\enzoj\OneDrive\Documents\GitHub\SimpleBlockchain-in-Rust\simple_blockchain\target\debug\simple_blockchain.exe: C:\Users\enzoj\OneDrive\Documents\GitHub\SimpleBlockchain-in-Rust\simple_blockchain\src\main.rs C:\Users\enzoj\OneDrive\Documents\GitHub\SimpleBlockchain-in-Rust\simple_blockchain\src\p2p.rs
