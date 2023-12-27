7d684a3837588b4dbddf375f5ec9aa745f8b5388
Enj <99920180+0xenj@users.noreply.github.com>
2023-12-27 12:28:22 +0100

    debug

--- simple_blockchain/target/debug/deps/simple_blockchain.d
+++ simple_blockchain/target/debug/deps/simple_blockchain.d
@@ -1,5 +1,6 @@
-C:\Users\enzoj\OneDrive\Documents\GitHub\SimpleBlockchain-in-Rust\simple_blockchain\target\debug\deps\simple_blockchain.exe: src\main.rs
+C:\Users\enzoj\OneDrive\Documents\GitHub\SimpleBlockchain-in-Rust\simple_blockchain\target\debug\deps\simple_blockchain.exe: src\main.rs src\p2p.rs
 
-C:\Users\enzoj\OneDrive\Documents\GitHub\SimpleBlockchain-in-Rust\simple_blockchain\target\debug\deps\simple_blockchain.d: src\main.rs
+C:\Users\enzoj\OneDrive\Documents\GitHub\SimpleBlockchain-in-Rust\simple_blockchain\target\debug\deps\simple_blockchain.d: src\main.rs src\p2p.rs
 
 src\main.rs:
+src\p2p.rs:
